d0de91b0ee3fc4f78bdddcd604a0563d1e930e69
lucho1 <luchosuaya@hotmail.com>
2021-04-12 23:57:10 +0200

    Added File Dialogues

--- Source/Core/Utils/FileStringUtils.cpp
+++ Source/Core/Utils/FileStringUtils.cpp
@@ -1,5 +1,12 @@
 #include "FileStringUtils.h"
 
+// --- To get usage of windows file dialogs ---
+#include <commdlg.h>
+// --- To attach file dialogs to the engine's window ---
+#define GLFW_EXPOSE_NATIVE_WIN32 // If defined, we can get Win32 functionalities we need
+#include <GLFW/glfw3native.h>
+
+
 
 // ------------------------------------------------------------------------------
 namespace StringUtils
@@ -56,6 +63,7 @@ namespace StringUtils
 // ------------------------------------------------------------------------------
 namespace FileUtils
 {
+    // ----- Files Standard Functions -----
     std::string FileUtils::ReadTextFile(const char* filepath)
     {
         FILE* file = fopen(filepath, "rb");
@@ -125,5 +133,78 @@ namespace FileUtils
         char* str = (char*)StringUtils::PushBytes(path.c_str(), length);
         StringUtils::PushChar(0);
         return str;
+    }
+
+
+
+    // ----- Files Dialogues Functions -----
+    std::string FileDialogs::OpenFile(const char* filter, GLFWwindow* window)
+    {
+        // -- Initialize OPENFILENAME to 0 (Common Dialog Box Structure) --
+        OPENFILENAMEA open_file_name;
+        ZeroMemory(&open_file_name, sizeof(OPENFILENAME));
+
+        // -- Set OPENFILENAME --
+        open_file_name.lStructSize = sizeof(OPENFILENAME); // Size
+        
+        open_file_name.hwndOwner = glfwGetWin32Window(window); // Owner of dialog window to engine's window
+
+        // -- Buffer File --
+        CHAR file[260] = { 0 };
+        open_file_name.lpstrFile = file;
+        open_file_name.nMaxFile = sizeof(file);
+        open_file_name.lpstrFilter = filter;
+        open_file_name.nFilterIndex = 1;
+        open_file_name.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR; // If this is not here, it will change the working directory
+
+        // -- Check for current directory and set it if exists --
+        CHAR directory[260] = { 0 };
+        if (GetCurrentDirectoryA(256, directory))
+            open_file_name.lpstrInitialDir = directory;
+
+        // -- If file (Ascii) is open (exists), return it --
+        if (GetOpenFileNameA(&open_file_name) == TRUE)
+            return open_file_name.lpstrFile;
+
+        return std::string();
+    }
+
+    std::string FileDialogs::SaveFile(const char* filter, const char* filename, GLFWwindow* window)
+    {
+        // -- Initialize OPENFILENAME to 0 (Common Dialog Box Structure) --
+        OPENFILENAMEA open_file_name;
+        ZeroMemory(&open_file_name, sizeof(OPENFILENAME));
+
+        
+
+        // -- Set OPENFILENAME --
+        open_file_name.lStructSize = sizeof(OPENFILENAME); // Size
+        open_file_name.hwndOwner = glfwGetWin32Window(window); // Owner of dialog window to engine's window		
+
+        // -- Buffer File --
+        CHAR file[260] = { 0 };
+        if (filename)
+        {
+            std::strncpy(file, filename, sizeof(file));	// Copy filename to file[]
+            file[sizeof(file) - 1] = 0;					// Add null-terminator at the end (in case filename is >= than the file[] max size)
+        }
+
+        open_file_name.lpstrFile = file;
+        open_file_name.nMaxFile = sizeof(file);
+        open_file_name.lpstrFilter = filter;
+        open_file_name.nFilterIndex = 1;
+        open_file_name.lpstrDefExt = strchr(filter, '\0') + 1;								// Default extension (gets it from filter)
+        open_file_name.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR;	// If this is not here, it will change the working directory
+
+        // -- Check for current directory and set it if exists --
+        CHAR directory[260] = { 0 };
+        if (GetCurrentDirectoryA(256, directory))
+            open_file_name.lpstrInitialDir = directory;
+
+        // -- If file (Ascii) is open (exists), return it --
+        if (GetSaveFileNameA(&open_file_name) == TRUE)
+            return open_file_name.lpstrFile;
+
+        return std::string();
     }
 }
--- Source/Core/Utils/FileStringUtils.h
+++ Source/Core/Utils/FileStringUtils.h
@@ -2,9 +2,11 @@
 #define _FILESTRINGUTILS_H_
 
 #include "../Globals.h"
+#include <GLFW/glfw3.h>
 
 namespace FileUtils
 {
+	// --- File Standard Functions ---
 	// Reads file & returns string with its contents
 	std::string ReadTextFile(const char* filepath);
 	std::string MakePath(const std::string& dir, const std::string& filename);
@@ -12,6 +14,19 @@ namespace FileUtils
 	// Last time file was modified. Check for file modifications for hot reloads.
 	uint64 GetFileLastWriteTimestamp(const char* filepath);
 	std::string GetDirectoryPart(const std::string& path);
+
+
+	// --- Files Dialogues ---
+	class FileDialogs
+	{
+	public:
+
+		// If cancelled, returns empty string
+		static std::string OpenFile(const char* filter, GLFWwindow* window);
+
+		// If cancelled, returns empty string
+		static std::string SaveFile(const char* filter, const char* filename, GLFWwindow* window);
+	};
 }
 
 namespace StringUtils
